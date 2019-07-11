e21beb4b5263db31ea0bb21224cec105bc45d591
Mebrahtom Guesh <guesh@student.chalmers.se>
2019-07-11 14:45:09 +0200

    Add and configure tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,18 +5,18 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
+const pumlEncoder = require('../extension');
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 // import * as vscode from 'vscode';
 // import * as myExtension from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
+suite("Test suite", function () {
 
     // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("Get diagram url", function() {
+        assert.equal(pumlEncoder.getURL("Bob -> Alice : hello"),
+         "http://www.plantuml.com/plantuml/img/SoWkIImgAStDuNBAJrBGjLDmpCbCJbMmKiX8pSd9vt98pKi1IW80");
     });
 });
